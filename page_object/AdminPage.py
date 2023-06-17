48fcef40aa08ce32c19da2f9280aa3c32e431968
v.efimova <vilka.ru@mail.ru>
2023-06-17 13:52:58 +0300

    project

--- page_object/AdminPage.py
+++ page_object/AdminPage.py
@@ -80,11 +80,11 @@ class AdminPage:
 
     def field_product_name_serch(self):
         return WebDriverWait(self.driver, time_sleep.VERY_SLOW, poll_frequency=1).until(
-            EC.presence_of_element_located((By.XPATH, ".//input[@name= 'filter_name']")))
+            EC.presence_of_element_located((By.XPATH, ".//input[@name='filter_name']")))
 
     def click_filter(self):
         btn_filter = WebDriverWait(self.driver, time_sleep.VERY_SLOW, poll_frequency=1).until(
-            EC.element_to_be_clickable((By.XPATH, ".//button[@id = 'button-filter']")))
+            EC.element_to_be_clickable((By.XPATH, ".//button[@id ='button-filter']")))
         btn_filter.click()
 
     def click_checkbox_element_for_delete(self):
